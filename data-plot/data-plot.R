bdc6a9247c17f85e05f61cd3c0311e1a4c77b9a5
Arseniy Khvorov <khvorov45@gmail.com>
2020-07-31 08:27:48 +1000

    rename year_shifted to x_position

--- data-plot/data-plot.R
+++ data-plot/data-plot.R
@@ -12,10 +12,10 @@ source(file.path(data_dir, "read_data.R"))
 plot_one_pid <- function(data, key) {
   virus_names <- data %>%
     group_by(virus) %>%
-    summarise(year_shifted = unique(year_shifted), .groups = "drop")
+    summarise(x_position = unique(x_position), .groups = "drop")
   plot <- data %>%
     ggplot(
-      aes(year_shifted, titre,
+      aes(x_position, titre,
         col = as.factor(timepoint),
         shape = as.factor(timepoint),
         linetype = as.factor(timepoint)
@@ -34,12 +34,12 @@ plot_one_pid <- function(data, key) {
     scale_y_log10("Titre", breaks = 5 * 2^(0:10)) +
     scale_x_continuous(
       "Virus",
-      breaks = virus_names$year_shifted, labels = virus_names$virus,
+      breaks = virus_names$x_position, labels = virus_names$virus,
       expand = c(0, 0.5)
     ) +
     coord_cartesian(
       clip = "off", ylim = c(5, max(data$titre)),
-      xlim = c(min(data$year_shifted), max(data$year_shifted))
+      xlim = c(min(data$x_position), max(data$x_position))
     ) +
     geom_line() +
     geom_point() +
@@ -65,14 +65,14 @@ hi <- read_data("hi") %>%
   # Each pid should have one virus label per year
   group_by(pid, virus_year) %>%
   mutate(
-    year_shifted =
+    x_position =
       virus_year + (map_int(virus, ~ which(.x == unique(virus))) - 1) /
         length(unique(virus))
   ) %>%
   ungroup()
 
 indiv_hi_plots <- hi %>%
-  # filter(pid == "HIA15611") %>%
+  filter(pid == "HIA15611") %>%
   group_by(pid, group, sex, age) %>%
   group_map(plot_one_pid)
 
