99c2eb61aa33a885e399cc236eb6da4f088a2034
Arseniy Khvorov <khvorov45@gmail.com>
2020-10-16 12:01:11 +1100

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -4,28 +4,28 @@ SHARED="/home/khvorova/vidrlwhoflu/Research"
 OBJ1="$SHARED/BAA 75D301-10-R-67835/Analyses/Objective 1"
 OBJ2="$SHARED/BAA 75D301-10-R-67835/Analyses/Objective 2"
 
-rm -rf "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 2/Israel/indiv-hi-2" "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 2/Israel/indiv-hi-2-alt"
-cp -R /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-2 /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-2-alt "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 2/Israel"
+rm -rf "$OBJ2/Israel/indiv-hi-2" "$OBJ2/Israel/indiv-hi-2-alt"
+cp -R $PROJ/data-plot/indiv-hi-2 $PROJ/data-plot/indiv-hi-2-alt "$OBJ2/Israel"
 
-rm -rf "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 2/Israel/indiv-hi-2-bwyears" "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 2/Israel/indiv-hi-2-bwyears-alt"
-cp -R /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-2-bwyears /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-2-bwyears-alt "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 2/Israel"
+rm -rf "$OBJ2/Israel/indiv-hi-2-bwyears" "$OBJ2/Israel/indiv-hi-2-bwyears-alt"
+cp -R $PROJ/data-plot/indiv-hi-2-bwyears $PROJ/data-plot/indiv-hi-2-bwyears-alt "$OBJ2/Israel"
 
-rm -rf "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 1/Israel/indiv-hi" "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 1/Israel/indiv-hi-alt"
-cp -R /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-alt "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 1/Israel"
+rm -rf "$OBJ1/Israel/indiv-hi" "$OBJ1/Israel/indiv-hi-alt"
+cp -R $PROJ/data-plot/indiv-hi $PROJ/data-plot/indiv-hi-alt "$OBJ1/Israel"
 
-rm -rf "/home/khvorova/vidrlwhoflu/Research/Ha Nam/landscape-plots/indiv-hi-hanam" "/home/khvorova/vidrlwhoflu/Research/Ha Nam/landscape-plots/indiv-hi-hanam-alt"
-cp -R /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-hanam /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-hanam-alt "/home/khvorova/vidrlwhoflu/Research/Ha Nam/landscape-plots"
+rm -rf "$SHARED/Ha Nam/landscape-plots/indiv-hi-hanam" "$SHARED/Ha Nam/landscape-plots/indiv-hi-hanam-alt"
+cp -R $PROJ/data-plot/indiv-hi-hanam $PROJ/data-plot/indiv-hi-hanam-alt "$SHARED/Ha Nam/landscape-plots"
 
-rm -rf "/home/khvorova/vidrlwhoflu/Research/2016.003 RMH HCW Study/Analyses/landscape-plots/indiv-hi-rmh-hcw" "/home/khvorova/vidrlwhoflu/Research/2016.003 RMH HCW Study/Analyses/landscape-plots/indiv-hi-rmh-hcw-alt"
-cp -R /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-rmh-hcw /home/khvorova/Projects/ab-landscape/data-plot/indiv-hi-rmh-hcw-alt "/home/khvorova/vidrlwhoflu/Research/2016.003 RMH HCW Study/Analyses/landscape-plots"
+rm -rf "$SHARED/2016.003 RMH HCW Study/Analyses/landscape-plots/indiv-hi-rmh-hcw" "$SHARED/2016.003 RMH HCW Study/Analyses/landscape-plots/indiv-hi-rmh-hcw-alt"
+cp -R $PROJ/data-plot/indiv-hi-rmh-hcw $PROJ/data-plot/indiv-hi-rmh-hcw-alt "$SHARED/2016.003 RMH HCW Study/Analyses/landscape-plots"
 
 
-cp /home/khvorova/Projects/ab-landscape/simple-diff/simple-diff-rmh-hcw.png "/home/khvorova/vidrlwhoflu/Research/2016.003 RMH HCW Study/Analyses/landscape-plots/simple-diff-rmh-hcw.png"
+cp $PROJ/simple-diff/simple-diff-rmh-hcw.png "$SHARED/2016.003 RMH HCW Study/Analyses/landscape-plots/simple-diff-rmh-hcw.png"
 
-cp /home/khvorova/Projects/ab-landscape/simple-diff/simple-diff-hanam.png "/home/khvorova/vidrlwhoflu/Research/Ha Nam/landscape-plots/simple-diff-hanam.png"
-cp /home/khvorova/Projects/ab-landscape/simple-diff/simple-diff-hanam-d280.png "/home/khvorova/vidrlwhoflu/Research/Ha Nam/landscape-plots/simple-diff-hanam-d280.png"
+cp $PROJ/simple-diff/simple-diff-hanam.png "$SHARED/Ha Nam/landscape-plots/simple-diff-hanam.png"
+cp $PROJ/simple-diff/simple-diff-hanam-d280.png "$SHARED/Ha Nam/landscape-plots/simple-diff-hanam-d280.png"
 
-cp /home/khvorova/Projects/ab-landscape/simple-diff/simple-diff.png "/home/khvorova/vidrlwhoflu/Research/BAA 75D301-10-R-67835/Analyses/Objective 1/Israel/simple-diff.png"
+cp $PROJ/simple-diff/simple-diff.png "$OBJ1/Israel/simple-diff.png"
 
 
 rm -rf "$OBJ2/drop-off/*"
