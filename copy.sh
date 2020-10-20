fbdf6e76722f93237d89242e3a3758b78e88f42c
Arseniy Khvorov <khvorov45@gmail.com>
2020-10-21 08:29:28 +1100

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -11,8 +11,13 @@ HANAM="$SHARED/Ha Nam"
 # Objective 1 -----------------------------------------------------------------
 
 # HI plots
-rm -rf "$OBJ1/Israel/indiv-hi" "$OBJ1/Israel/indiv-hi-alt"
-cp -R "$PROJ/data-plot/indiv-hi" "$PROJ/data-plot/indiv-hi-alt" "$OBJ1/Israel"
+rm -rf "$OBJ1/Israel/indiv-hi" \
+    "$OBJ1/Israel/indiv-hi-alt" \
+    "$OBJ1/Israel/indiv-hi-contour"
+cp -R "$PROJ/data-plot/indiv-hi" \
+    "$PROJ/data-plot/indiv-hi-alt" \
+    "$PROJ/data-plot/indiv-hi-contour" \
+    "$OBJ1/Israel"
 
 # Differences
 cp $PROJ/simple-diff/simple-diff.png "$OBJ1/Israel/simple-diff.png"
@@ -20,31 +25,56 @@ cp $PROJ/simple-diff/simple-diff.png "$OBJ1/Israel/simple-diff.png"
 # Objective 2 -----------------------------------------------------------------
 
 # HI plots
-rm -rf "$OBJ2/Israel/indiv-hi-2" "$OBJ2/Israel/indiv-hi-2-alt" "$OBJ2/Israel/indiv-hi-2-bwyears" "$OBJ2/Israel/indiv-hi-2-bwyears-alt"
-cp -R "$PROJ/data-plot/indiv-hi-2" "$PROJ/data-plot/indiv-hi-2-alt" "$PROJ/data-plot/indiv-hi-2-bwyears" "$PROJ/data-plot/indiv-hi-2-bwyears-alt" "$OBJ2/Israel"
+rm -rf "$OBJ2/Israel/indiv-hi-2" \
+    "$OBJ2/Israel/indiv-hi-2-alt" \
+    "$OBJ2/Israel/indiv-hi-2-bwyears" \
+    "$OBJ2/Israel/indiv-hi-2-bwyears-alt"
+cp -R "$PROJ/data-plot/indiv-hi-2" \
+    "$PROJ/data-plot/indiv-hi-2-alt" \
+    "$PROJ/data-plot/indiv-hi-2-bwyears" \
+    "$PROJ/data-plot/indiv-hi-2-bwyears-alt" \
+    "$OBJ2/Israel"
 
 # Drop-off evaluation
 rm -rf "$OBJ2/drop-off/*"
-cp "$PROJ/drop-off/spag.png" "$PROJ/drop-off/spag-even.png" "$PROJ/drop-off/spag-facets.png" "$PROJ/drop-off/vac-resp.png" "$PROJ/drop-off/vac-resp.csv" "$PROJ/drop-off/vac-resp-scatter.png" "$PROJ/drop-off/vac-resp-cat.png" "$PROJ/drop-off/vac-resp-vacc.png" "$PROJ/drop-off/vac-resp-age.png" "$OBJ2/drop-off"
+cp "$PROJ/drop-off/spag.png" \
+    "$PROJ/drop-off/spag-even.png" \
+    "$PROJ/drop-off/spag-facets.png" \
+    "$PROJ/drop-off/vac-resp.png" \
+    "$PROJ/drop-off/vac-resp.csv" \
+    "$PROJ/drop-off/vac-resp-scatter.png" \
+    "$PROJ/drop-off/vac-resp-cat.png" \
+    "$PROJ/drop-off/vac-resp-vacc.png" \
+    "$PROJ/drop-off/vac-resp-age.png" \
+    "$OBJ2/drop-off"
 
 # Hanam -----------------------------------------------------------------------
 
 # HI plots
-rm -rf "$HANAM/landscape-plots/indiv-hi-hanam" "$HANAM/landscape-plots/indiv-hi-hanam-alt"
-cp -R "$PROJ/data-plot/indiv-hi-hanam" "$PROJ/data-plot/indiv-hi-hanam-alt" "$HANAM/landscape-plots"
+rm -rf "$HANAM/landscape-plots/indiv-hi-hanam" \
+    "$HANAM/landscape-plots/indiv-hi-hanam-alt"
+cp -R "$PROJ/data-plot/indiv-hi-hanam" \
+    "$PROJ/data-plot/indiv-hi-hanam-alt" \
+    "$HANAM/landscape-plots"
 
 # Differences
-cp "$PROJ/simple-diff/simple-diff-hanam.png" "$HANAM/landscape-plots/simple-diff-hanam.png"
-cp "$PROJ/simple-diff/simple-diff-hanam-d280.png" "$HANAM/landscape-plots/simple-diff-hanam-d280.png"
+cp "$PROJ/simple-diff/simple-diff-hanam.png" \
+    "$HANAM/landscape-plots/simple-diff-hanam.png"
+cp "$PROJ/simple-diff/simple-diff-hanam-d280.png" \
+    "$HANAM/landscape-plots/simple-diff-hanam-d280.png"
 
 # RMH -------------------------------------------------------------------------
 
 # HI plots
-rm -rf "$RMH/Analyses/landscape-plots/indiv-hi-rmh-hcw" "$RMH/Analyses/landscape-plots/indiv-hi-rmh-hcw-alt"
-cp -R "$PROJ/data-plot/indiv-hi-rmh-hcw" "$PROJ/data-plot/indiv-hi-rmh-hcw-alt" "$RMH/Analyses/landscape-plots"
+rm -rf "$RMH/Analyses/landscape-plots/indiv-hi-rmh-hcw" \
+    "$RMH/Analyses/landscape-plots/indiv-hi-rmh-hcw-alt"
+cp -R "$PROJ/data-plot/indiv-hi-rmh-hcw" \
+    "$PROJ/data-plot/indiv-hi-rmh-hcw-alt" \
+    "$RMH/Analyses/landscape-plots"
 
 # Differences
-cp "$PROJ/simple-diff/simple-diff-rmh-hcw.png" "$RMH/Analyses/landscape-plots/simple-diff-rmh-hcw.png"
+cp "$PROJ/simple-diff/simple-diff-rmh-hcw.png" \
+    "$RMH/Analyses/landscape-plots/simple-diff-rmh-hcw.png"
 
 # The whole project -----------------------------------------------------------
 
