ec0dd35304db2f75e9e54497eb135eeff12f99f7
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-08 11:28:44 +1100

    rework the cdc data

--- data/data.R
+++ data/data.R
@@ -72,6 +72,28 @@ save_data <- function(data, name) {
   write_csv(data, glue::glue("data/{name}.csv"))
 }
 
+compare_vectors <- function(vec1, vec2, vec1_lbl = "in1", vec2_lbl = "in2") {
+  in1 <- setdiff(vec1, vec2)
+  in2 <- setdiff(vec2, vec1)
+  if (length(in1) > length(in2)) {
+    longer <- in1
+    shorter <- in2
+  } else {
+    longer <- in2
+    shorter <- in1
+  }
+  shorter_padded <- c(sort(shorter), rep(NA, length(longer) - length(shorter)))
+  cmp <- tibble(sort(longer), shorter_padded)
+  if (length(in1) > length(in2)) {
+    colnames(cmp) <- c(vec1_lbl, vec2_lbl)
+  } else {
+    colnames(cmp) <- c(vec2_lbl, vec1_lbl)
+  }
+  cmp %>%
+    select(!!rlang::sym(vec1_lbl), !!rlang::sym(vec2_lbl)) %>%
+    filter(!is.na(!!rlang::sym(vec1_lbl)) | !is.na(!!rlang::sym(vec2_lbl)))
+}
+
 # Script ======================================================================
 
 # The map ---------------------------------------------------------------------
@@ -86,190 +108,164 @@ agmap <- read_raw_csv("miniH3.coords", col_types = cols()) %>%
 
 save_data(agmap, "map")
 
-# The raw CDC Israel data -----------------------------------------------------
+# CDC -------------------------------------------------------------------------
 
-viruses_raw <- read_raw("Viruses")
-dilutions_raw <- read_raw("VirusDiln_BT")
-sera_raw <- read_raw("Sera List")
-samples_raw <- read_raw("Obj1_Sample_info")
-hi_raw <- read_raw("HI")
+# Viruses
 
-# Select the required columns
+cdc_viruses_raw_obj1 <- read_raw("cdc-obj1/Viruses")
+cdc_viruses_raw_obj2 <- read_raw("cdc-obj2/Viruses")
 
-sera <- sera_raw %>%
-  select(sample = Sample_ID, pid = PID, timepoint_num = Timepoint) %>%
-  filter(timepoint_num %in% 1:3) %>%
-  mutate(timepoint = recode_3_timepoints(timepoint_num))
+fmt_cdc_viruses <- function(data) {
+  data %>%
+    select(
+      virus_full = Virus_Name,
+      virus_short = Short_name,
+      virus_n = VirusN,
+      virus_year = Year,
+      clade = Clade,
+      egg = Egg_Cell,
+    ) %>%
+    mutate(
+      virus_full = standardise_full_virus_name(virus_full),
+      virus_year = as.integer(virus_year),
+      clade = replace_na(clade, "(Missing)"),
+      egg = egg == 0,
+      # Use the Egg_Cell column as the source of truth for egg status
+      virus_short = str_replace(virus_short, "e$", "") %>%
+        if_else(egg, paste0(., "e"), .),
+      virus_full = str_replace(virus_full, "e$", "") %>%
+        if_else(egg, paste0(., "e"), .),
+    )
+}
 
-hi <- select(
-  hi_raw,
-  sample = Sample_ID, virus_n = VirusN, titre = Titer
-)
-viruses <- select(
-  viruses_raw,
-  virus_full = Virus_Name,
-  virus = Short_name, virus_n = VirusN, virus_year = Year, clade = Clade
-) %>%
-  mutate(
-    virus_full = standardise_full_virus_name(virus_full),
-    virus_year = as.integer(virus_year),
-    clade = replace_na(clade, "(Missing)"),
-    egg = str_detect(virus, "e$")
-  ) %>%
-  left_join(agmap, by = "virus_full")
+cdc_viruses_obj1 <- fmt_cdc_viruses(cdc_viruses_raw_obj1)
+cdc_viruses_obj2 <- cdc_viruses_raw_obj2 %>%
+  rename(Year = Virus_Year) %>%
+  fmt_cdc_viruses()
 
-# Check which viruses aren't on the map
-setdiff(viruses$virus_full, agmap$virus_full)
-setdiff(agmap$virus_full, viruses$virus_full)
+# The differences are:
+# - 'p' on the end
+# - hanam vs hanoi
+compare_vectors(cdc_viruses_obj1$virus_full, cdc_viruses_obj2$virus_full)
+# The differences are:
+# - 'p' on the end
+compare_vectors(cdc_viruses_obj1$virus_short, cdc_viruses_obj2$virus_short)
 
-participants <- select(
-  samples_raw,
-  pid = PID, group = `Case/Control`, sex = Sex, age = Age
+# See if they would match with the map
+# - 'p' on the end
+compare_vectors(
+  cdc_viruses_obj1$virus_full, agmap$virus_full, "ob1", "map"
+) %>% print(n = 99)
+
+# - 'p' on the end
+# - hanam vs hanoi
+compare_vectors(
+  cdc_viruses_obj2$virus_full, agmap$virus_full, "ob2", "map"
+) %>% print(n = 99)
+
+cdc_viruses_obj1 %>% filter(!complete.cases(.))
+cdc_viruses_obj2 %>% filter(!complete.cases(.))
+
+# They may be united if the names are sorted out
+save_data(cdc_viruses_obj1, "cdc-virus-obj1")
+save_data(cdc_viruses_obj2, "cdc-virus-obj2")
+
+# Participants for objective 1
+
+cdc_hi_time_obj1 <- read_raw("cdc-obj1/HI_timecourse")
+
+cdc_participants_obj1 <- select(
+  cdc_hi_time_obj1,
+  pid = PID, group = `Case/Control`, sex = Sex, yob = YoB
 ) %>%
   distinct(pid, .keep_all = TRUE) %>%
   mutate(
-    age_lab = paste("Age:", age),
-    # It'd be good to know actual dates of birth
-    year_of_birth = floor(2019.5 - age),
-    group = recode(group, "Case" = "Infrequent", "Control" = "Frequent")
-  )
+    group = str_replace(group, "Vax", "") %>% str_trim()
+  ) %>%
+  filter(pid != "KK38") # Annette
 
-# HI results
+cdc_participants_obj1 %>% filter(!complete.cases(.))
 
-# See if the sample id's match
-setdiff(hi$sample, sera$sample)
-setdiff(sera$sample, hi$sample)
+save_data(cdc_participants_obj1, "cdc-participant-obj1")
 
-# See if virus numbers are as unique as names
-equally_unique("virus", "virus_n", viruses)
+# Participants for objective 2
 
-# Extra variables for HI results
-hi_extra <- hi %>%
-  mutate(
-    logtitre = log(titre),
-    logtitre_mid = if_else(titre == 5L, logtitre, logtitre + log(2) / 2)
-  )
+cdc_hi_prior_vacc_obj2 <- read_raw("cdc-obj2/prior_vacc")
 
-# See if virus numbers match
-setdiff(viruses$virus_n, hi$virus_n)
-setdiff(hi$virus_n, viruses$virus_n)
+cdc_participants_obj2 <- cdc_hi_prior_vacc_obj2 %>%
+  select(pid = study_id, sex = Sex, yob = yob) %>%
+  distinct(pid, .keep_all = TRUE)
 
-# See if pid's match
-setdiff(sera$pid, participants$pid) # KK38 is Annette, exclude that
-setdiff(participants$pid, sera$pid)
+cdc_participants_obj2 %>% filter(!complete.cases(.))
 
-# Join the relevant bits of HI data
-hi_full <- inner_join(hi_extra, sera, by = "sample") %>%
-  inner_join(viruses, by = "virus_n") %>%
-  inner_join(participants, by = "pid") %>%
-  filter(!str_detect(sample, "KK38")) %>% # Because Annette's
-  # Vaccine strain
-  mutate(vaccine_strain = virus_n == 40)
+save_data(cdc_participants_obj2, "cdc-participant-obj2")
 
-save_csv(hi_full, "hi")
+# Prior vaccinations for objective 2
 
-# Israel objective 2 ----------------------------------------------------------
-
-hi_2 <- read_raw("Obj2_timecourse_200904_complete")
-
-# Pull blood collection dates
-hi_2_dates <- hi_2 %>%
-  select(contains("Blood_Draw_date"), pid = PID) %>%
+cdc_vacc_hist_obj2 <- cdc_hi_prior_vacc_obj2 %>%
+  select(pid = study_id, contains("Vacc_")) %>%
+  select(pid, matches("c$")) %>%
   pivot_longer(
-    contains("Blood_Draw_date"),
-    names_to = "timepoint_global",
-    values_to = "timepoint_date"
+    contains("Vacc_"),
+    names_to = "vacc_year", values_to = "vacc_status"
   ) %>%
   mutate(
-    timepoint_global = str_replace(
-      timepoint_global, "^time\\s?(\\d)\\.Blood_Draw_date$", "\\1"
-    ) %>% as.integer(),
-    timepoint_date = lubridate::as_date(timepoint_date)
+    vacc_year = str_replace(vacc_year, "Vacc_(\\d{4})c", "\\1") %>%
+      as.integer()
+  )
+
+cdc_vacc_hist_obj2 %>% filter(!complete.cases(.))
+unique(cdc_vacc_hist_obj2$vacc_year)
+unique(cdc_vacc_hist_obj2$vacc_status)
+
+compare_vectors(cdc_vacc_hist_obj2$pid, cdc_participants_obj2$pid)
+
+save_data(cdc_vacc_hist_obj2, "cdc-vacc-hist-obj2")
+
+# HI for objective 1
+
+cdc_hi_obj1 <- cdc_hi_time_obj1 %>%
+  select(pid = PID, contains("Titer")) %>%
+  pivot_longer(
+    contains("Titer"),
+    names_to = "timepoint", values_to = "titre"
   ) %>%
-  distinct(pid, timepoint_global, timepoint_date) %>%
-  # Make sure there is one blood collection point per id per time
-  mutate(rown = row_number()) %>%
-  group_by(pid, timepoint_global) %>%
-  filter(rown == first(rown)) %>%
-  select(-rown) %>%
-  # Impute the missing dates by looking at the average time b/w timepoints
-  mutate(timepoint_date_imputed = is.na(timepoint_date))
+  mutate(
+    timepoint = recode(
+      timepoint,
+      "Pre.Titer" = "prevax",
+      "Post.Titer" = "postvax",
+      "PostS.Titer" = "postseas"
+    )
+  ) %>%
+  filter(pid != "KK38")
 
-# Need to impute dates sequentially and in both directions
-t_first <- min(hi_2_dates$timepoint_global)
-t_last <- max(hi_2_dates$timepoint_global)
-for (i in c(t_first:t_last, t_last:t_first)) {
-  hi_2_dates <- hi_2_dates %>%
-    group_by(timepoint_global) %>%
-    mutate(
-      timepoint_date = if_else(
-        is.na(timepoint_date),
-        lead(timepoint_date) - mean(
-          lead(timepoint_date) - timepoint_date,
-          na.rm = TRUE
-        ),
-        timepoint_date
-      ),
-      timepoint_date = if_else(
-        is.na(timepoint_date),
-        lag(timepoint_date) + mean(
-          timepoint_date - lag(timepoint_date),
-          na.rm = TRUE
-        ),
-        timepoint_date
-      )
-    ) %>%
-    ungroup()
-}
+cdc_hi_obj1 %>% filter(!complete.cases(.))
 
-hi_2_final <- hi_2 %>%
+compare_vectors(cdc_hi_obj1$pid, cdc_participants_obj1$pid)
+
+save_data(cdc_hi_obj1, "cdc-hi-obj1")
+
+# HI for objective 2
+
+cdc_hi_obj2_raw <- read_raw("cdc-obj2/HIresult", guess_max = 1e5)
+
+cdc_hi_obj2 <- cdc_hi_obj2_raw %>%
   select(
-    pid = PID, sex = Sex, age = Age, virus_year = Virus_Year, virus_n = VirusN,
-    n5y_prior_vacc = n5Y_prior_vacc,
-    virus = Short_name,
-    cluster,
-    contains("L2HI"),
-  ) %>%
-  pivot_longer(
-    contains("L2HI"),
-    names_to = "timepoint_global", values_to = "titre"
+    pid = PID, study_year = SpecYearN, timepoint = DrawN, titre = Titer
   ) %>%
   mutate(
-    titre = as.integer(round(2^titre, 0)),
-    logtitre = log(titre),
-    logtitre_mid = if_else(titre == 5L, logtitre, logtitre + log(2) / 2),
-    timepoint_global = str_replace(
-      timepoint_global, "^time\\s?(\\d)\\.L2HI$", "\\1"
-    ) %>% as.integer(),
-    timepoint_num = ((timepoint_global - 1) %% 3) + 1,
-    timepoint = recode_3_timepoints(timepoint_num),
-    age_lab = paste("Age:", age),
-    clade = paste("cluster", cluster),
-    study_year = ceiling(timepoint_global / 3),
-    study_year_lab = glue::glue(
-      "Year {study_year} 20{15 + study_year}/{16 + study_year}"
-    ),
-    n5y_prior_vacc_lab = paste0("Vac in past 5 years: ", n5y_prior_vacc),
-    # It'd be good to know the actual dob
-    year_of_birth = floor(2019.5 - age),
-    vaccine_strain = (virus_n == 5 & study_year %in% c(1, 2)) |
-      (virus_n == 40 & study_year == 3),
-    egg = str_detect(virus, "e$")
-  )
+    timepoint = recode(
+      timepoint, "1" = "prevax", "2" = "postvax", "3" = "postseas"
+    )
+  ) %>%
+  filter(!is.na(titre))
 
-hi_2_final_dates <- inner_join(
-  hi_2_final, hi_2_dates,
-  by = c("pid", "timepoint_global")
-)
+cdc_hi_obj2 %>% filter(!complete.cases(.))
 
-# Pull the long virus names from objective 1
-setdiff(viruses$virus, unique(hi_2_final_dates$virus))
+compare_vectors(cdc_hi_obj2$pid, cdc_participants_obj2$pid)
 
-hi_2_final_viruses <- hi_2_final_dates %>%
-  inner_join(select(viruses, virus, virus_full), by = "virus") %>%
-  left_join(agmap, by = "virus_full")
-
-save_csv(hi_2_final_viruses, "hi-obj2")
+save_data(cdc_hi_obj2, "cdc-hi-obj2")
 
 # The Hanam dataset Annette gave me -------------------------------------------
 
