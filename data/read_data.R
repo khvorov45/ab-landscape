14f3bbdbb8847a8890bd3685341d4f31841e82cb
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-18 08:31:46 +1100

    Update read_data.R

--- data/read_data.R
+++ data/read_data.R
@@ -1,20 +1,8 @@
 read_data <- function(name) {
-  common <- list(
-    titre = col_integer(),
-    timepoint_num = col_integer(),
-    virus_year = col_integer(),
-    clade = col_character()
-  )
+  common <- list()
   specific <- list(
     "hi-hanam" = list(
       timepoint = col_factor(c("BL", "d4", "d7", "d14", "d21", "d280"))
-    ),
-    "hi-obj2" = list(
-      timepoint = col_factor(c("Pre-vax", "Post-vax", "Post-season"))
-    ),
-    "hi" = list(
-      timepoint = col_factor(c("Pre-vax", "Post-vax", "Post-season")),
-      group = col_factor(c("Frequent", "Infrequent"))
     ),
     "hi-rmh-hcw" = list(
       timepoint = col_factor(c("Pre-vax", "Post-vax", "Post-season")),
