eb65ddf947983a7c9ea8e9a4b63dae4c3471af34
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-24 14:16:03 +1100

    Update read_data.R

--- data/read_data.R
+++ data/read_data.R
@@ -1,12 +1,14 @@
 read_data <- function(name) {
   common <- list()
   cdc_timepoint <- col_factor(c("prevax", "postvax", "postseas"))
+  cdc_timepoint2 <- col_factor(c("Pre-vax", "Post-vax", "Post-season"))
+
   specific <- list(
     "hi-hanam" = list(
       timepoint = col_factor(c("BL", "d4", "d7", "d14", "d21", "d280"))
     ),
     "cdc-obj1-hi" = list(
-      timepoint = cdc_timepoint
+      timepoint = cdc_timepoint2
     ),
     "cdc-hi-obj2" = list(
       timepoint = cdc_timepoint
