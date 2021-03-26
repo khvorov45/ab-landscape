32e7a16aa1fd67f0ce49e5f67ea599342ed0a275
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-26 12:18:17 +1100

    add objective 4 export

---
+++ data-raw/cdc-obj4/export.sh
@@ -0,0 +1,9 @@
+# Use mdbtools to export all tables in csv
+# sudo apt-get install mdbtools
+
+# Read schema
+mdb-schema CDC_Obj4.accdb | 
+# Find table names within that schema
+grep -oP "CREATE TABLE \[\K.+(?=\])" | 
+# Export each table into its own file under NIGGCWserol folder
+while read line ; do mdb-export CDC_Obj4.accdb "$line" > "$line.csv" ; done
