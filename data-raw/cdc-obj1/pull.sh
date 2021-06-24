d537ff0b0674bfe15573117d574ddf3146e532cc
Arseniy Khvorov <khvorov45@gmail.com>
2021-06-24 12:21:21 +1000

    add pull script

---
+++ data-raw/cdc-obj1/pull.sh
@@ -0,0 +1,2 @@
+cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/CDC HCW study/Operational/Objective 1/CDC_Obj1.accdb" \
+    "/home/khvorova/Projects/ab-landscape/data-raw/cdc-obj1/CDC_Obj1.accdb"
